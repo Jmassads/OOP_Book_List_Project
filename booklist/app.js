e2ca19e00d4e3fb50586bcd06413b58b73652c4b
Julia Assad <juliasajah85@gmail.com>
2018-11-28 17:03:39 +0100

    Validation and Success

--- booklist/app.js
+++ booklist/app.js
@@ -29,6 +29,32 @@ UI.prototype.addBooktoList = function (book) {
 
 }
 
+// Show Alert
+
+UI.prototype.showAlert = function (message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+
+  div.appendChild(document.createTextNode(message));
+
+  // Get parent 
+  const container = document.querySelector('.container');
+
+  // Get form
+  const form = document.querySelector('#book-form');
+
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 seconds
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -50,13 +76,25 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   // Instantiate UI
   const ui = new UI();
 
-  // Add book to list
+  // Validate
 
-  ui.addBooktoList(book);
+  if (title === '' || author === '' || isbn === '') {
+    // error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
 
-  // Clear fields
+    ui.addBooktoList(book);
 
-  ui.clearFields();
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+
+    // Clear fields
+
+    ui.clearFields();
+  }
+
+
 
   e.preventDefault();
 
